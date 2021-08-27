e891737fbf07eb7b61bcf79a87243084374a9d1c
ishanz22 <62241228+ishanz22@users.noreply.github.com>
2021-08-27 09:52:09 +0530

    OrderForm Refactor

--- POS/src/lk/ijse/pos/dao/OrderDAOImpl.java
+++ POS/src/lk/ijse/pos/dao/OrderDAOImpl.java
@@ -5,11 +5,6 @@ import lk.ijse.pos.model.Orders;
 
 import java.util.ArrayList;
 
-*/
-/**
- * @author : Sanu Vithanage
- * @since : 0.1.0
- **//*
 
 public class OrderDAOImpl {
 
@@ -34,4 +29,5 @@ public class OrderDAOImpl {
     }
 
 }
+
 */
